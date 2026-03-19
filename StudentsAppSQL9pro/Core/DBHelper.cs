8fca4d0fa218b90fc1d9d15d2bff9a4f15f17c2f
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-19 20:59:57 +0200

    chore: add db helper utility

---
+++ StudentsAppSQL9pro/Core/DBHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace StudentsAppSQL9pro.Core
+{
+    public class DBHelper
+    {
+        private readonly string? _connectionString;
+        public DBHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+            // configuration["ConnectionStrings:DefaultConnection"] 
+        }
+
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+
+    }
+}
