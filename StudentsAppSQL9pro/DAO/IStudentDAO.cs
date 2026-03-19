227665a0185f7d7667e0efefe149d43ea280c277
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-19 21:01:38 +0200

    chore: add dao impl with all CRUD API implemented

---
+++ StudentsAppSQL9pro/DAO/IStudentDAO.cs
@@ -0,0 +1,19 @@
+﻿using StudentsAppSQL9pro.Models;
+
+namespace StudentsAppSQL9pro.DAO
+{
+    public interface IStudentDAO
+    {
+        Student? Insert(Student student);
+        
+        void Update(Student student);
+
+        void Delete(int id);
+
+        Student? GetById(int id);
+
+        List<Student> GetAll();
+
+
+    }
+}
