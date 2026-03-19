59694061e82fe529dc73dada7426f09bb0fff8fe
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-19 21:01:00 +0200

    chore: add dao for student

---
+++ StudentsAppSQL9pro/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentsAppSQL9pro.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+
+
+    }
+    
+}
